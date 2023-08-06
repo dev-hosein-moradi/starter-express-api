bef4978135dc2c27e47ba16c35e2fc06fa176c6f
hoseinmoradi <moradih7379@gmail.com>
2023-08-06 19:09:28 +0330

    upd

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = express();
 /* for user ===> hosein */
 app.all("/hosein", (req, res) => {
   res.send(
-    "   vless://a6cbd9e1-ebc5-42c1-a173-3f450cb93268@5.42.74.172:5423?type=tcp&security=reality&sni=www.speedtest.net&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&flow=xtls-rprx-vision&fp=chrome#hosein-sup-swe2   " +
-      "  vless://924ec820-fd34-4578-8f64-bcd4017c1cfd@5.42.74.172:8443?type=tcp&security=reality&sni=www.speedtest.net&pbk=UK7qxWWGfRQcQfwaGpHnqmmqqJBut4jxve8AeDDJ2UI&flow=xtls-rprx-vision&fp=chrome#hosein2-sup-swe2  " +
-      "   vless://7ed4c39e-b00a-482d-bf21-eac0a84ed0df@5.42.74.172:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=Xx7HyT5wE7716x-UMPNRgh4Gea5WEETeIdC_400uqFo&sid=f0664a9846f0f983&type=tcp&headerType=none#singbox-swed2      " +
+    "   vless://c7089570-39a0-4687-a2db-66ec46cc4403@5.42.74.172:8443?type=tcp&security=reality&sni=www.speedtest.net&pbk=UtL7E0Gmxj3X5JdcPAutpTRKo7K2hugkR0vwk2XroUM&flow=xtls-rprx-vision&fp=chrome#hosein-swe   " +
       "     vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk     "
   );
 });
@@ -14,7 +12,7 @@ app.all("/hosein", (req, res) => {
 /* for user ===> mohamad */
 app.all("/mohamad", (req, res) => {
   res.send(
-    "   vless://5747de69-af3a-4be8-d9e9-4aaa11620a52@5.42.74.172:27199?type=tcp&security=reality&sni=www.speedtest.net&pbk=rYH4wPTVzSwtpXgI3U7YxppIP6oudD-425vT7pyhj1w&flow=xtls-rprx-vision&fp=chrome#mohamad-sup-swe2   " +
+    "   vless://547a8342-a16b-49c2-c53b-f4f98b9b29e2@5.42.74.172:44314?type=tcp&security=reality&sni=www.speedtest.net&pbk=cDaDzPr3PlS3NM8lreHZbdo-Mhqz8vMBzMSkHXhGIUA&flow=xtls-rprx-vision&fp=chrome#mohamad-swe   " +
       "    vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
   );
 });
@@ -22,7 +20,7 @@ app.all("/mohamad", (req, res) => {
 /* for user ===> raheleh */
 app.all("/raheleh", (req, res) => {
   res.send(
-    "   vless://851c0e18-dcc5-4967-85ce-b07df704e13e@5.42.74.172:31641?type=tcp&security=reality&sni=www.speedtest.net&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&flow=xtls-rprx-vision&fp=chrome#raheleh-sup-swe2   " +
+    "   vless://b7865195-93f6-4d7c-a81e-c3c1553a909e@5.42.74.172:58845?type=tcp&security=reality&sni=www.speedtest.net&pbk=S-g0oP36DShii1uPOnZDSEhp_wQghX6h68PgMivOmD4&flow=xtls-rprx-vision&fp=chrome#raheleh-swe   " +
       "    vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
   );
 });
@@ -30,7 +28,7 @@ app.all("/raheleh", (req, res) => {
 /* for user ===> ghazal  */
 app.all("/ghazal", (req, res) => {
   res.send(
-    "   vless://f079b90f-bf74-444e-c691-094b6019501f@5.42.74.172:11120?type=tcp&security=reality&sni=www.speedtest.net&pbk=4comh-7Jm_wZXJQ5QiLSCbVGQIbMUzHUIBdb0aFtLzM&flow=xtls-rprx-vision&fp=chrome#ghazal-sup-swe2   " +
+    "   vless://4e66de00-bfe3-43ab-e43d-698a1711f32d@5.42.74.172:23960?type=tcp&security=reality&sni=www.speedtest.net&pbk=UK7qxWWGfRQcQfwaGpHnqmmqqJBut4jxve8AeDDJ2UI&flow=xtls-rprx-vision&fp=chrome#ghazal-swe   " +
       "    vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
   );
 });
@@ -38,14 +36,14 @@ app.all("/ghazal", (req, res) => {
 /* for user ===> somayeh ios */
 app.all("/somayeh", (req, res) => {
   res.send(
-    "   vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
+    "   vless://3827334d-ce8c-4dfe-c667-7369e60ed0e2@5.42.74.172:22254?type=tcp&security=reality&sni=www.speedtest.net&pbk=g-oxbqigzCaXqARxuyD2_vbTYeMD9zn8wnTo02S69QM&flow=xtls-rprx-vision&fp=chrome#somayeh-swe    "
   );
 });
 
 /* for user ===> yasna */
 app.all("/yasna", (req, res) => {
   res.send(
-    "   vless://61143991-ff75-407d-c715-f30ca1e33b5b@5.42.74.172:43450?type=tcp&security=reality&sni=www.speedtest.net&pbk=UtL7E0Gmxj3X5JdcPAutpTRKo7K2hugkR0vwk2XroUM&flow=xtls-rprx-vision&fp=chrome#yasna-sup-swe2   " +
+    "   vless://58e5725b-0dca-49fc-b458-0d6c831590a8@5.42.74.172:30042?type=tcp&security=reality&sni=www.speedtest.net&pbk=rYH4wPTVzSwtpXgI3U7YxppIP6oudD-425vT7pyhj1w&flow=xtls-rprx-vision&fp=chrome#yasna-swe   " +
       "   vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk     "
   );
 });
@@ -57,10 +55,11 @@ app.all("/khatereh", (req, res) => {
       "    vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
   );
 });
+
 /* for user ===> radan */
 app.all("/radan", (req, res) => {
   res.send(
-    "   vless://1a76601b-2d83-4539-e9ea-68c3a557a925@5.42.74.172:50268?type=tcp&security=reality&sni=www.speedtest.net&pbk=R2gKMF0Tetlnesc1pPkZH9NaOeehw-f5_U9JKG_cLjU&flow=xtls-rprx-vision&fp=chrome#radan-sup-swe2     " +
+    "   vless://077306e1-f250-4ccb-fd16-3626140d03c4@5.42.74.172:52455?type=tcp&security=reality&sni=www.speedtest.net&pbk=W9BjX6YmCIVsjhKMlz233Yoe0xcf0SVHfvPKqbf3vCg&flow=xtls-rprx-vision&fp=chrome#radan-swe     " +
       "     vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk     "
   );
 });
@@ -68,7 +67,7 @@ app.all("/radan", (req, res) => {
 /* for user ===> mohsen */
 app.all("/mohsen", (req, res) => {
   res.send(
-    "   vless://ac4b5f26-9046-448a-e17c-17526cf019db@5.42.74.172:59048?type=tcp&security=reality&sni=www.speedtest.net&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&flow=xtls-rprx-vision&fp=chrome#mohsen-sup-swe2   " +
+    "   vless://afda19f8-f764-4c7e-a74b-c1dca39ffb9f@5.42.74.172:10627?type=tcp&security=reality&sni=www.speedtest.net&pbk=qhTzYYIgBzDLNYR79oxftqdo1kzL-1_hGJKfqrOliCY&flow=xtls-rprx-vision&fp=chrome#mohsen-swe   " +
       "     vless://060f15fb-ccc1-49b2-ea82-755574413fa5@80.85.241.1:443?type=tcp&security=reality&sni=www.speedtest.net&pbk=Z3ZGnAOdKkzJ07gR_7_0k9_iTTFP6paDOrqx1rN2LU4&flow=xtls-rprx-vision&fp=chrome#sup-helsk    "
   );
 });
