895b003ece605c7aa7b132dde635468c855e80df
hoseinmoradi <moradih7379@gmail.com>
2023-08-16 16:20:13 +0330

    upd

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ app.all("/mohsen", (req, res) => {
   );
 });
 
+/* for user ===> khatereh */
+app.all("/khatereh", (req, res) => {
+  res.send(
+    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+  );
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`running on: ${process.env.PORT || 3000}`);
 });
