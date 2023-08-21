0de57d73dc16a3b86ee73f1d6b3126a4e0ab6b8d
hoseinmoradi <moradih7379@gmail.com>
2023-08-21 23:30:05 +0330

    upd

--- index.js
+++ index.js
@@ -4,63 +4,42 @@ const app = express();
 /* for user ===> hosein */
 app.all("/hosein", (req, res) => {
   res.send(
-    "    vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox    " 
-  );
-});
-
-/* for user ===> mohamad */
-app.all("/mohamad", (req, res) => {
-  res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "    vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze    " 
   );
 });
 
 /* for user ===> raheleh */
 app.all("/raheleh", (req, res) => {
   res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
   );
 });
 
 /* for user ===> ghazal  */
 app.all("/ghazal", (req, res) => {
   res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
-  );
-});
-
-/* for user ===> somayeh ios */
-app.all("/somayeh", (req, res) => {
-  res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
   );
 });
 
 /* for user ===> yasna */
 app.all("/yasna", (req, res) => {
   res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
   );
 });
 
 /* for user ===> radan */
 app.all("/radan", (req, res) => {
   res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
   );
 });
 
 /* for user ===> mohsen */
 app.all("/mohsen", (req, res) => {
   res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
-  );
-});
-
-/* for user ===> khatereh */
-app.all("/khatereh", (req, res) => {
-  res.send(
-    "vless://b660fc80-e0ae-481d-81b1-bc7b8672ff3d@94.228.164.72:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.speedtest.net&fp=chrome&pbk=qF002EZCDlyZhCA1olOx-8hC2ndUO6nNyUPbmx_ZWH8&sid=f29d9bb07e9fe1c8&type=tcp&headerType=none#swe-singbox"
+    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
   );
 });
 
