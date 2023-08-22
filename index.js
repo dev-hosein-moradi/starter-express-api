708847c03595ec6cbae1407d60c67d7eb495a9e0
hoseinmoradi <moradih7379@gmail.com>
2023-08-22 10:58:58 +0330

    upd

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 
 /* for user ===> hosein */
-app.all("/hosein", (req, res) => {
+app.all("/secure/hosein", (req, res) => {
   res.send(
-    "    vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze    " 
+    "    vless://fd441b5f-4ee6-45f9-bd80-fe3c4ca19b99@promo.devhosein.fun:443?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1%2Ch3&allowInsecure=1#swed-tls-tcp-    " +
+    "    vless://03793fc9-971a-4f90-a389-d5ca26394f72@94.228.164.72:8443?type=tcp&security=reality&fp=chrome&pbk=prWsK7f7fb-LKRYVkfui5sFxGTYgJtnlW_pSjVznL30&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=7b866315&spx=%2F#swed-reality-    " +
+    "    vless://e2e29142-3f57-4f59-b6ba-e202ecf7e97b@promo.devhosein.fun:2053?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1%2Ch3&allowInsecure=1#swed-tls-grpc-    "
+  );
+});
+
+/* for user ===> raheleh */
+app.all("/secure/somayeh", (req, res) => {
+  res.send(
+    "vless://e2e29142-3f57-4f59-b6ba-e202ecf7e97b@promo.devhosein.fun:2053?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1%2Ch3&allowInsecure=1#swed-tls-grpc-"
   );
 });
 
