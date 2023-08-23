767ea13e6fe40bc1215d1b6994bc3a1ef5f6aace
hoseinmoradi <moradih7379@gmail.com>
2023-08-23 16:31:24 +0330

    upd

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 /* for user ===> hosein */
 app.all("/secure/hosein", (req, res) => {
   res.send(
-    "    vless://13aec8a7-0d0e-462d-a9fe-d37d959a4155@promo.devhosein.fun:443?type=tcp&security=reality&fp=firefox&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-    " 
+    "    vless://13aec8a7-0d0e-462d-a9fe-d37d959a4155@promo.devhosein.fun:443?type=tcp&security=reality&fp=firefox&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-    " +
+      "   vless://4bcc350c-ecee-427b-90ac-79b158a1040f@swedd.devhosein.fun:2053?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tcp-tls-1xeg6h5p    " +
+      "    vless://abba430b-fbdb-4dba-8cfa-4835bab1edd6@swedd.devhosein.fun:2096?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-grpc-    " +
+      "    vless://09a4fc6d-d0cd-4f8f-969c-f09fa3b3f5ae@swedd.devhosein.fun:8443?type=ws&path=%2F&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-ws-    "
   );
 });
 
