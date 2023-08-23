54b76a469f7f08d8bff47557fb4a431f90a7d9d2
hoseinmoradi <moradih7379@gmail.com>
2023-08-23 17:30:21 +0330

    upd

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const app = express();
 /* for user ===> hosein */
 app.all("/secure/hosein", (req, res) => {
   res.send(
-    "    vless://13aec8a7-0d0e-462d-a9fe-d37d959a4155@promo.devhosein.fun:443?type=tcp&security=reality&fp=firefox&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-    " +
-      "   vless://4bcc350c-ecee-427b-90ac-79b158a1040f@swedd.devhosein.fun:2053?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tcp-tls-1xeg6h5p    " +
-      "    vless://abba430b-fbdb-4dba-8cfa-4835bab1edd6@swedd.devhosein.fun:2096?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-grpc-    " +
-      "    vless://09a4fc6d-d0cd-4f8f-969c-f09fa3b3f5ae@swedd.devhosein.fun:8443?type=ws&path=%2F&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-ws-    "
+    "    vless://13aec8a7-0d0e-462d-a9fe-d37d959a4155@promo.devhosein.fun:443?type=tcp&security=reality&fp=chrome&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-hosein-reality-1    " +
+      "   vless://4bcc350c-ecee-427b-90ac-79b158a1040f@swedd.devhosein.fun:2053?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tcp-tls-hosein-tcp-tls    " +
+      "    vless://abba430b-fbdb-4dba-8cfa-4835bab1edd6@swedd.devhosein.fun:2096?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-grpc-hosein-tls-grpc    " +
+      "    vless://acdec7fe-b7e1-4168-a409-97c744342357@swedd.devhosein.fun:8443?type=tcp&security=reality&fp=chrome&pbk=d5Qjpf4cxkyVGKbVOZDf5hSBqti4Ptiw1IKfM_-0XSI&sni=www.theverge.com&flow=xtls-rprx-vision&sid=fdf6b718&spx=%2F#swedd-reality-2-hosein-reality-2    "
+  );
+});
+
+/* for user ===> ghazal */
+app.all("/secure/ghazal", (req, res) => {
+  res.send(
+    "    vless://299ed7c6-e4b8-4ffb-b054-5f9c9812e705@promo.devhosein.fun:443?type=tcp&security=reality&fp=chrome&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-ghazal-reality-1    " +
+      "   vless://676d53b8-1458-412d-ad6c-00ecdaac1ae4@swedd.devhosein.fun:2053?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tcp-tls-ghazal-tls-tcp    " +
+      "    vless://8576bce2-fd9d-46bc-af1c-e81718aa9eeb@swedd.devhosein.fun:2096?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-grpc-ghazal-tls-grpc    " +
+      "    vless://6dfe52ea-ec42-4999-982e-9af64c673156@swedd.devhosein.fun:8443?type=tcp&security=reality&fp=chrome&pbk=d5Qjpf4cxkyVGKbVOZDf5hSBqti4Ptiw1IKfM_-0XSI&sni=www.theverge.com&flow=xtls-rprx-vision&sid=fdf6b718&spx=%2F#swedd-reality-2-ghazal-reality-2    "
   );
 });
 
 /* for user ===> raheleh */
-app.all("/secure/somayeh", (req, res) => {
+app.all("/secure/raheleh", (req, res) => {
   res.send(
-    "vless://03793fc9-971a-4f90-a389-d5ca26394f72@94.228.164.72:8443?type=tcp&security=reality&fp=chrome&pbk=prWsK7f7fb-LKRYVkfui5sFxGTYgJtnlW_pSjVznL30&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=7b866315&spx=%2F#swed-reality-"
-  );
-});
-
-/* for user ===> radan */
-app.all("/radan", (req, res) => {
-  res.send(
-    "vless://d5d50475-af79-47e6-ae27-feb47aba35f3@msc.devhosein.fun:443?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1#russia-46u90yze"
+    "    vless://a225b38c-092c-4e71-b865-41ec30f07f5f@promo.devhosein.fun:443?type=tcp&security=reality&fp=chrome&pbk=jIgX4Ing36bwl82iz4dbXgEExBnMQtGu1ITLWyFxbCI&sni=www.speedtest.net&flow=xtls-rprx-vision&sid=24747170&spx=%2F#swed-reality-raheleh-reality-1    " +
+      "   vless://31f2baa1-80cf-436e-8ed6-f30fcbeaf029@swedd.devhosein.fun:2053?type=tcp&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tcp-tls-raheleh-tls-tcp    " +
+      "    vless://b7a88062-6df5-41e0-bfff-d789c4ddfe23@swedd.devhosein.fun:2096?type=grpc&serviceName=&security=tls&fp=chrome&alpn=h2%2Chttp%2F1.1&allowInsecure=1#swedd-tls-grpc-raheleh-tls-grpc    " +
+      "    vless://823e79b8-8354-4100-9340-5ba664ef9240@swedd.devhosein.fun:8443?type=tcp&security=reality&fp=chrome&pbk=d5Qjpf4cxkyVGKbVOZDf5hSBqti4Ptiw1IKfM_-0XSI&sni=www.theverge.com&flow=xtls-rprx-vision&sid=fdf6b718&spx=%2F#swedd-reality-2-raheleh-reality-2    "
   );
 });
 
